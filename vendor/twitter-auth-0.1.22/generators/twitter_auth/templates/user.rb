6d18d8d5b0ec4f2a7715e2b4ff25bf48b5c2aa8c
SproutingSeed <sproutingseed@gmail.com>
2009-07-04 01:27:14 -0700

    second

--- vendor/twitter-auth-0.1.22/generators/twitter_auth/templates/user.rb
+++ vendor/twitter-auth-0.1.22/generators/twitter_auth/templates/user.rb
@@ -2,6 +2,4 @@ class User < TwitterAuth::GenericUser
   # Extend and define your user model as you see fit.
   # All of the authentication logic is handled by the 
   # parent TwitterAuth::GenericUser class.
-  attr_accessor :pass
-  has_many :todos
 end
